c950b613301f475eca960279ed098d6fb1818f5c
Ramiro Cerdá <50562689+ramiro-c@users.noreply.github.com>
2026-06-06 17:31:10 -0300

    feat: add email error hierarchy

---
+++ projects/01-adapter-email/src/errors.ts
@@ -0,0 +1,27 @@
+export class EmailSendError extends Error {
+  constructor(message: string, public readonly cause?: unknown) {
+    super(message);
+    this.name = "EmailSendError";
+  }
+}
+
+export class TransientError extends EmailSendError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "TransientError";
+  }
+}
+
+export class PermanentError extends EmailSendError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "PermanentError";
+  }
+}
+
+export class CircuitBreakerOpenError extends EmailSendError {
+  constructor() {
+    super("Circuit breaker is open");
+    this.name = "CircuitBreakerOpenError";
+  }
+}
